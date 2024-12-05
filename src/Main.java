86b37da28e60276c5161917094a58fa1e4dd4b01
BT_4N2_23 <few@>
2024-12-05 13:17:58 -0500

    12/5 edit

--- src/Main.java
+++ src/Main.java
@@ -13,17 +13,39 @@ public class Main {
 
     public static int getAnswer(ArrayList<String> fileData) {
         ArrayList<Integer> levels = new ArrayList<>();
+        int safeCount = 0;
         for (int i = 0; i < fileData.size(); i++) {
             String line = fileData.get(i);
             String[] num = line.split(" ");
             for(int j = 0; j < num.length;j++){
                 levels.add(j,Integer.parseInt(num[j]));
-                boolean safe = false;
             }
-
-            System.out.println(levels);
+            boolean s = checkSafe(levels);
+            if (s){
+                safeCount++;
+            }
+            levels.clear();
         }
-        return -1;
+        return safeCount;
+    }
+    public static boolean checkSafe(ArrayList<Integer> levels){
+        boolean safe = false;
+        boolean in = false;
+        boolean de = false;
+        for (int i = 1; i < levels.size();i++){
+            int a = levels.get(i-1);
+            int b = levels.get(i);
+            System.out.println(Math.abs(a-b) + "");
+            if (Math.abs(a-b) <= 3 && !de || Math.abs(a-b) > 1 && !de){
+                safe = true;
+                in = true;
+            } else if (Math.abs(b-a) <= 3 && !in || Math.abs(b-a) > 1 && !in) {
+                safe = true;
+                de = true;
+            } else return safe;
+            System.out.println(safe);
+        }
+        return safe;
     }
 
     public static ArrayList<String> getFileData(String fileName) {
